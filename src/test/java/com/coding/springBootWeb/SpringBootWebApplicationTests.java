96464038cecc112266970ce43c06bd94feb47451
Lareb <lareb131213@gmail.com>
2024-12-28 00:42:27 +0530

    Chapter 2.1

---
+++ src/test/java/com/coding/springBootWeb/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.springBootWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
